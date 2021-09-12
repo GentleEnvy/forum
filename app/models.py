b10d1cdbdb0da88ee9fccd602e67d0b6ad262dc2
Envy <envy15@mail.ru>
2021-09-12 20:09:33 +0300

    add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+__all__ = ['User', 'Tag', 'Discussion', 'Comment']
+
+
+class Tag(models.Model):
+    title = models.TextField()
+
+
+class Discussion(models.Model):
+    title = models.TextField()
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
+
+
+class Comment(models.Model):
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
+    text = models.TextField()
