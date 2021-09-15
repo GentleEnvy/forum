40e762c6fe07a619b73cc469665b1b6754fa0eaf
GentleEnvy <envy15@nail.ru>
2021-09-15 12:48:18 +0300

    update models (+ migrations)

--- app/models.py
+++ app/models.py
@@ -5,17 +5,23 @@ __all__ = ['User', 'Tag', 'Discussion', 'Comment']
 
 
 class Tag(models.Model):
-    title = models.TextField()
+    title = models.TextField(unique=True)
+
+
+class Forum(models.Model):
+    title = models.TextField(unique=True)
 
 
 class Discussion(models.Model):
     title = models.TextField()
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
 
 
 class Comment(models.Model):
     discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_created=True)
     text = models.TextField()
